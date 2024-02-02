ea133a628a9cc0e355ccbab13b2450ee8aad53c1
lucassantossdev <lucas.santos@severinoapp.com>
2024-02-02 11:00:13 -0300

    frist commit :tada:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ApiServicesService } from './service/api-services.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ApiServicesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/service/api-services.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiServicesService {
+
+  serve:  string = "";
+  constructor(private http:HttpClient) { }
+
+
+  Api(dados:any, api:string){
+    const httpOpitions={
+      headrs: new HttpHeaders({'Content-Type':'application/json'})
+    };
+    let url = this.serve + api;
+    return this.http.post(url, JSON.stringify(dados), );
+  }
+}
